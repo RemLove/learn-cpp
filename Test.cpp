833eb28554e2b8f6bf9a09dad70bf9186b24e40e
Rem_Lovr <113937604@qq.com>
2026-06-06 21:46:51 +0800

    学习引用：引用做参数、引用做返回值及相关练习
    添加 Test1.cpp，在 Test.cpp 中练习 swap、count1、count2 等引用相关函数。

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,47 @@
 #include<iostream>
-int main()
+using namespace std;
+//1.引用做参数
+static void swap(int& a, int& b)
 {
-	std::cout << "hello world" << std::endl;
+	int temp = a;
+	a = b;
+	b = temp;
+}
+int count1(int n)
+{
+	n++;
+	return n;
+}
+//2.引用做返回值
+static int& count2(int n)
+{
+	n++;
+	return n;
+}
+static int add(int a, int b)
+{
+	return a + b;
+}
+int main1()
+{
+	//std::cout << "hello world" << std::endl;
+	//int a = 10;
+	//int& b = a;
+	////double& c = a;//错误
+	//const double& d = a;
 	int a = 10;
-	int& b = a;
-	//double& c = a;//错误
-	const double& d = a;
+	int b = 20;
+	swap(a, b);//可以，直接传的a和b
+	int c = add(a, b);
+
+	////////////
+	//int n1 = count(1);//错误，返回的是n的拷贝，不能修改，应该加const
+	const int n1 = count1(1);
+	int n2 = count2(1);
+
+	int a1 = 1;
+	int& b1 = a1;
+	int c1 = 2;
+	b1 = c1;
+	printf("%d", a1);
 }
---
+++ Test1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+	void push(int x)
+	{
+		//实现
+	}
+	void pop();
+public:
+	int age = 0;
+
+};
+void Student::pop()
+{
+	//实现
+}
+
+
+int main()
+{
+	Student sl;
+	sl.age = 10;
+	//sl.name = "Yue Wang";
+}
