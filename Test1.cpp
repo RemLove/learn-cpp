4d852418a8f70a6798ce0cda51b5c3e6c67a82ea
Rem_Lovr <113937604@qq.com>
2026-06-07 21:53:51 +0800

    学习类和对象：构造函数、析构函数、运算符重载

--- Test1.cpp
+++ Test1.cpp
@@ -3,24 +3,41 @@ using namespace std;
 
 class Student
 {
-	void push(int x)
-	{
-		//实现
-	}
-	void pop();
 public:
-	int age = 0;
-
+	Student(int year=1,int month=1 ,int day=1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void print()
+	{
+		cout << _year << endl;
+		cout << _month << endl;
+		cout << _day << endl;
+	}
+	~Student()
+	{
+		cout << "hello" << endl;
+	}
+	bool operator>(const Student& sl)
+	{
+		return _year > sl._year;
+	}
+	int _age;
+private:
+	char name[20] = "Yue Wang";
+	int _year;
+	int _month;
+	int _day;
 };
-void Student::pop()
-{
-	//实现
-}
-
-
 int main()
 {
-	Student sl;
-	sl.age = 10;
+	Student sl1(2026, 6, 7);
+	sl1.print();
+	Student sl2(100);
+	sl2.print();
+	sl1.operator>(sl2);///
 	//sl.name = "Yue Wang";
+	Student* p = NULL;
 }
